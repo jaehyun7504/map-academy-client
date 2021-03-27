5812e1e891597130989e77ecc1699e4903f8e99f
Jaehyeon Kim <jaehyun7504@gmail.com>
2021-03-27 19:47:00 +0900

    Changed redirect case

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -1,17 +1,17 @@
-import React, { useState, useContext } from "react";
-import { Link, withRouter } from "react-router-dom";
-import { TokenDispatchContext } from "../contexts/token.context";
+import React, { useState, useContext } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { TokenDispatchContext } from '../contexts/token.context';
 import {
   IsAuthContext,
   IsAuthDispatchContext,
-} from "../contexts/isAuth.context";
-import useStyles from "../styles/Header.styles";
-import Navigation from "./Navigation.component";
-import Login from "./Login.component";
-import Reset from "./Reset.component";
-import Drawer from "./Drawer.component";
+} from '../contexts/isAuth.context';
+import useStyles from '../styles/Header.styles';
+import Navigation from './Navigation.component';
+import Login from './Login.component';
+import Reset from './Reset.component';
+import Drawer from './Drawer.component';
 
-function Header({ history }) {
+function Header({ match, history }) {
   const tDispatch = useContext(TokenDispatchContext);
   const isAuth = useContext(IsAuthContext);
   const aDispatch = useContext(IsAuthDispatchContext);
@@ -30,54 +30,56 @@ function Header({ history }) {
   };
   const hideReset = () => setReset(false);
 
-  const submit = (input) => {
-    fetch("https://www.mapacademyapi.com/api/login", {
-      method: "POST",
+  const submit = input => {
+    fetch('https://www.mapacademyapi.com/api/login', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(input),
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.message === "error") {
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === 'error') {
           return alert(data.error);
         }
-        tDispatch({ type: "CREATE", token: data.data.token });
-        aDispatch({ type: "TOGGLE" });
-        window.localStorage.setItem("token", data.data.token);
-        window.localStorage.setItem("isAuth", true);
+        tDispatch({ type: 'CREATE', token: data.data.token });
+        aDispatch({ type: 'TOGGLE' });
+        window.localStorage.setItem('token', data.data.token);
+        window.localStorage.setItem('isAuth', true);
         setShow(!show);
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   };
-  const submitReset = (input) => {
-    fetch("https://www.mapacademyapi.com/api/reset", {
-      method: "POST",
+  const submitReset = input => {
+    fetch('https://www.mapacademyapi.com/api/reset', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(input),
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.message === "error") {
+      .then(res => res.json())
+      .then(data => {
+        if (data.message === 'error') {
           return alert(data.error);
         }
         alert(
-          "이메일을 확인하세요. 이메일이 오지 않은 경우, 스팸 보관함을 확인하세요."
+          '이메일을 확인하세요. 이메일이 오지 않은 경우, 스팸 보관함을 확인하세요.'
         );
         hideReset();
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   };
 
   const handleSignout = () => {
-    tDispatch({ type: "DELETE" });
-    aDispatch({ type: "TOGGLE" });
-    window.localStorage.removeItem("token");
-    window.localStorage.removeItem("isAuth");
-    history.push("/about");
+    tDispatch({ type: 'DELETE' });
+    aDispatch({ type: 'TOGGLE' });
+    window.localStorage.removeItem('token');
+    window.localStorage.removeItem('isAuth');
+    if (match.url.startsWith('/create') || match.url.startsWith('/update')) {
+      history.push('/about');
+    }
   };
 
   return (
