5c4d9bd25a40791f90aba393eb907f76313674e2
Jaehyeon Kim <jaehyun7504@gmail.com>
2021-03-28 16:36:19 +0900

    Blocked editing page when signed out

--- src/components/Edit.component.js
+++ src/components/Edit.component.js
@@ -1,6 +1,7 @@
-import React, { useContext, useState, useEffect } from "react";
-import { TokenContext } from "../contexts/token.context";
-import useStyles from "../styles/Edit.styles";
+import React, { useContext, useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { TokenContext } from '../contexts/token.context';
+import useStyles from '../styles/Edit.styles';
 
 function Edit({ hasImage, isUpdating, match, history }) {
   const token = useContext(TokenContext);
@@ -8,70 +9,72 @@ function Edit({ hasImage, isUpdating, match, history }) {
   const classes = useStyles();
 
   const [state, setState] = useState({
-    title: "",
-    body: "",
-    image: "",
+    title: '',
+    body: '',
+    image: '',
   });
-  const content = !hasImage ? "notices" : "articles";
+  const content = !hasImage ? 'notices' : 'articles';
 
   useEffect(() => {
     if (isUpdating) {
       fetch(`https://www.mapacademyapi.com/api/${content}/${match.params.id}`)
-        .then((res) => res.json())
-        .then((data) => {
+        .then(res => res.json())
+        .then(data => {
           setState({
             title: data.data.title,
             body: data.data.body,
           });
         })
-        .catch((err) => console.error(err));
+        .catch(err => console.error(err));
     }
   }, [isUpdating, content, match.params.id]);
 
-  const handleChange = (e) => {
+  if (!token) return <Redirect to="/about" />;
+
+  const handleChange = e => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
-  const handleFileChange = (e) => {
+  const handleFileChange = e => {
     setState({ ...state, image: e.target.files[0] });
   };
 
-  const editContent = (formData) => {
+  const editContent = formData => {
     fetch(
       `https://www.mapacademyapi.com/api/${content}/${
-        isUpdating ? match.params.id : ""
+        isUpdating ? match.params.id : ''
       }`,
       {
-        method: !isUpdating ? "POST" : "PATCH",
+        method: !isUpdating ? 'POST' : 'PATCH',
         headers: {
-          Authorization: "Bearer " + token,
+          Authorization: 'Bearer ' + token,
         },
         body: formData,
       }
     )
-      .then((res) => {
+      .then(res => {
         setState({
-          title: "",
-          body: "",
+          title: '',
+          body: '',
         });
         if (hasImage) {
           setState({ ...state, image: null });
-          document.getElementById("image").value = "";
+          document.getElementById('image').value = '';
         }
         history.push(`/${content}`);
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     if (!state.title || !state.body) {
-      return alert("내용을 입력해 주세요.");
+      return alert('내용을 입력해 주세요.');
     }
     const formData = new FormData();
-    formData.append("title", state.title);
-    formData.append("body", state.body);
+    formData.append('title', state.title);
+    formData.append('body', state.body);
     if (hasImage && state.image) {
-      formData.append("image", state.image, state.image.name);
+      formData.append('image', state.image, state.image.name);
     }
     editContent(formData);
   };
